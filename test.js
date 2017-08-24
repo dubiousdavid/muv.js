f21b805a4215b73d8abf1f21894584095a4bdba9
David Sargeant <david@dsargeant.com>
2017-08-23 22:15:52 -0400

    Fixed tests.

--- test.js
+++ test.js
@@ -1,33 +1,50 @@
-import test from 'ava'
-import { convertToHyperScript } from './muv'
-import toHtml from 'snabbdom-to-html'
+import test from 'ava';
+import { convertToHyperScript } from './muv';
+import toHtml from 'snabbdom-to-html';
 
-let emptyNode = {sel:undefined,data:{},children:undefined,text:undefined,elm:undefined,key:undefined}
+let emptyNode = {
+  sel: undefined,
+  data: {},
+  children: undefined,
+  text: undefined,
+  elm: undefined,
+  key: undefined
+};
 
 test('convertToHyperScript: text', t => {
-  t.is(convertToHyperScript('Hello'), 'Hello')
-})
+  t.is(convertToHyperScript('Hello'), 'Hello');
+});
 
 test('convertToHyperScript: sel', t => {
-  let hs = convertToHyperScript(['a'])
-  t.deepEqual(hs, {...emptyNode, sel: 'a'})
-  t.is(toHtml(hs), '<a></a>')
-})
+  let hs = convertToHyperScript(['a']);
+  t.deepEqual(hs, { ...emptyNode, sel: 'a' });
+  t.is(toHtml(hs), '<a></a>');
+});
 
 test('convertToHyperScript: sel, data, text', t => {
-  let hs = convertToHyperScript(['a', {props: {href: 'http://todomvc.com'}}, 'TodoMVC'])
-  t.deepEqual(hs, {...emptyNode, sel: 'a', data: {props: {href: 'http://todomvc.com'}}, text: 'TodoMVC'})
-  t.is(toHtml(hs), '<a href="http://todomvc.com">TodoMVC</a>')
-})
+  let hs = convertToHyperScript(['a', { props: { href: 'http://todomvc.com' } }, 'TodoMVC']);
+  t.deepEqual(hs, {
+    ...emptyNode,
+    sel: 'a',
+    data: { props: { href: 'http://todomvc.com' } },
+    children: [{ ...emptyNode, text: 'TodoMVC', data: undefined }]
+  });
+  t.is(toHtml(hs), '<a href="http://todomvc.com">TodoMVC</a>');
+});
 
 test('convertToHyperScript: sel, children', t => {
-  let hs = convertToHyperScript(['p', {}, [ ['div', {}, [ 42 ]], 'Hello' ]])
-  t.deepEqual(
-    hs,
-    {...emptyNode, sel: 'p', children: [
-      {...emptyNode, sel: 'div', children: [
-        {...emptyNode, text: 42, data: undefined}]},
-      {...emptyNode, text: 'Hello', data: undefined}]}
-  )
-  t.is(toHtml(hs), '<p><div>42</div>Hello</p>')
-})
+  let hs = convertToHyperScript(['p', {}, ['div', {}, 42], 'Hello']);
+  t.deepEqual(hs, {
+    ...emptyNode,
+    sel: 'p',
+    children: [
+      {
+        ...emptyNode,
+        sel: 'div',
+        children: [{ ...emptyNode, text: 42, data: undefined }]
+      },
+      { ...emptyNode, text: 'Hello', data: undefined }
+    ]
+  });
+  t.is(toHtml(hs), '<p><div>42</div>Hello</p>');
+});
